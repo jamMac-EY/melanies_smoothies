a62827162e4d2db388fa767eda9f27645bb43b80
jamMac-EY <James.Mackie1@uk.ey.com>
2025-09-16 11:12:22 +0100

    Update streamlit_app.py
    Move SmoothieFroot into IF statement

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 # Import python packages
 import streamlit as st
+import requests
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -26,6 +27,8 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     #st.write(ingredients_string)
 
@@ -39,9 +42,3 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         msg = "Your Smoothie is ordered, " + name_on_order + "!"
         st.success(msg, icon="✅")
-
-# New section to display SmoothieFroot nutrition information
-import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-# st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
